b75e30860eafb090974eb6e7c574fe551c009b74
Matt Nemitz <matthew.nemitz@habito.com>
2022-06-13 13:30:33 +0100

    Format comments, add SPDX license ID

--- smithy-mode.el
+++ smithy-mode.el
@@ -1,31 +1,33 @@
 ;;; smithy-mode.el --- Major mode for editing Smithy IDL files
 
-;; Copyright (C) 2022 Matt Nemitz
+;;; Copyright (C) 2022 Matt Nemitz
 
-;; Version: 0.1.0
-;; Author: Matt Nemitz <matt.nemitz@gmail.com>
-;; Maintainer: Matt Nemitz <matt.nemitz@gmail.com>
-;; URL: http://github.com/mnemitz/smithy-mode
-;; Keywords: tools, languages, smithy, IDL, amazon
-;; Package-Requires: ((emacs "26.1"))
+;;; Version: 0.1.1
+;;; Author: Matt Nemitz <matt.nemitz@gmail.com>
+;;; Maintainer: Matt Nemitz <matt.nemitz@gmail.com>
+;;; URL: http://github.com/mnemitz/smithy-mode
+;;; Keywords: tools, languages, smithy, IDL, amazon
+;;; Package-Requires: ((emacs "26.1"))
+;;; SPDX-License-Identifier: GPL-3.0-only
 
-;; This file is not part of GNU Emacs.
+;;; This file is not part of GNU Emacs.
 
 ;;; Commentary:
 
-;; Provides font-lock for the Smithy IDL (https://awslabs.github.io/smithy/)
-;; In the future this may be extended to more functionality,
-;; subject to progress on the Smithy Language Server implementation (https://github.com/awslabs/smithy-language-server)
+;;; Provides font-lock for the Smithy IDL (https://awslabs.github.io/smithy/)
+;;; This may be extended later to more functionality, subject to progress on the
+;;; Smithy Language Server implementation
+;;; (https://github.com/awslabs/smithy-language-server)
 
 ;;; Code:
 
-;; We leverage the C syntax table to get strings, comments, parentheses and braces
-;; matching and highlighting properly
+;;; We leverage the C syntax table to get strings, comments, parentheses and
+;;; braces matching and highlighting properly
 (require 'cc-langs)
 
 (defvar smithy-highlights
       (let* (
-            ;; define several category of keywords
+            ;;; define several category of keywords
              (x-keywords '("namespace" "use" "metadata" "apply" "member"))
              (x-types '("service" "operation" "resource" "map" "structure" "union" "set" "list"))
              (x-simple-type-names'("blob"
@@ -42,7 +44,7 @@
                                    "bigDecimal"
                                    "timestamp"))
              (x-node-keywords '("true" "false" "null"))
-            ;; generate regex string for each category of keywords
+            ;;; generate regex string for each category of keywords
              (x-keywords-regexp (regexp-opt x-keywords 'words))
              (x-traits-regexp "@\\w+\\(\\.\\w+\\)*\\(#\\w+\\)?")
              (x-types-regexp (regexp-opt x-types 'words))
@@ -57,8 +59,8 @@
           (,x-node-keywords-regexp . 'font-lock-keyword-face)
           (,x-control-statement-regexp . 'font-lock-preprocessor-face)
           (,x-traits-regexp . 'font-lock-function-name-face))))
-          ;; note: order above matters, because once colored, that part won't change.
-          ;; in general, put longer words first
+          ;;; note: order above matters, because once colored, that part won't change.
+          ;;; in general, put longer words first
           
 
 
